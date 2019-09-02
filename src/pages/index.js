072a588c7d9028071a076ae244a1f07d3ab9a543
mustardseeddesignco <bmoore@mustardseeddesign.co>
2019-09-02 12:17:48 -0400

    new push

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,32 @@
 import React from "react"
-import Header from '../@briodev/gatsby-theme-blog/components/header';
-
-
-
+import Header from "../@briodev/gatsby-theme-blog/components/header"
+import { Container } from "theme-ui"
+import styled from "@emotion/styled"
 
 export default function index() {
-    return (
-        <div>
-            <Header />
-            <h1>Hello World</h1>
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+
+      <HeroBox>
+        <HeroText>We Design Inspiration</HeroText>
+      </HeroBox>
+
+      <Container>
+        <h3>We Love To Design On Edge</h3>
+      </Container>
+    </div>
+  )
 }
 
+const HeroText = styled.h1`
+  color: black;
+  text-align: center;
+  font-size: 520%;
+`
 
-
-// export default () =>
-//     <div>
-       
-//         <h1>Mustard Seed Design Co.</h1>
-//     </div>
-
+const HeroBox = styled.div`
+  background-color: goldenrod;
+  padding: 4rem;
+  width: 100vw;
+`
